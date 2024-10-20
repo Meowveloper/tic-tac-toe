78107d55c3c77ec1351d85302bbc40093315a80f
Meowveloper <zwekhantaung22.tgi@gmail.com>
2024-10-20 22:33:14 +0700

    bored

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <ctime>
+
+void drawBoard(char *spaces);
+void playerMove(char *spaces, char player);
+void computerMove(char *spaces, char computer);
+void checkWinner(char *spaces, char player, char computer);
 
 int main ()
 {
     return 0;
+}
+
+void drawBoard(char *spaces)
+{
+
+}
+void playerMove(char *spaces, char player)
+{
+
+}
+void computerMove(char *spaces, char computer)
+{
+
+}
+void checkWinner(char *spaces, char player, char computer) 
+{
+
 }
